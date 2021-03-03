f1f87bea84051ead5efafe24a2769f2046eb6814
Kidy Lee <kidylee@gmail.com>
2021-03-03 20:35:14 +0800

    init

---
+++ init.el
@@ -0,0 +1,128 @@
+(setq inhibit-startup-message t)
+
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode 1)            ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "http://mirrors.cloud.tencent.com/elpa/melpa/")
+                         ("org" . "http://mirrors.cloud.tencent.com/elpa/org/")
+                         ("elpa" . "http://mirrors.cloud.tencent.com/elpa/gnu/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+;; Enable line number globally and disable for cerntain mode
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package try)
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(defalias 'list-buffers 'ibuffer-other-window)
+
+;; Window management
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
+    ))
+
+
+;; it looks like counsel is a requirement for swiper
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+
+
+(use-package ivy-rich
+  :after ivy
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("C-M-j" . 'counsel-switch-buffer)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (counsel-mode 1))
+
+(use-package avy
+  :ensure t
+  :bind ("M-s" . avy-goto-char))
+
+
+
+;; Helpful adds a lot of very helpful (get it?) information to Emacs’
+;; describe- command buffers. For example, if you use
+;; describe-function, you will not only get the documentation about
+;; the function, you will also see the source code of the function and
+;; where it gets used in other places in the Emacs configuration. It
+;; is very useful for figuring out how things work in Emacs.
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
