72997b64492794a837e4d27d88cafde3d824dc3f
Kidy Lee <kidylee@gmail.com>
2021-03-04 19:37:19 +0800

    add misc

--- init.el
+++ init.el
@@ -18,4 +18,18 @@
 (setq use-package-always-ensure t)
 
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(iedit multiple-cursors expand-region beacon org-bullets undo-tree which-key use-package try ivy-rich helpful counsel ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+(put 'narrow-to-region 'disabled nil)
