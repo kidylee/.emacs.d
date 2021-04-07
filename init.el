df21024e9a13e362e23a74d6e382cc1410d55284
Kidy Lee <kidylee@gmail.com>
2021-04-07 09:33:11 +0800

    add rg

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(iedit multiple-cursors expand-region beacon org-bullets undo-tree which-key use-package try ivy-rich helpful counsel ace-window)))
+   '(rg logview ivy-prescient flycheck company-box dap-mode lsp-ivy lsp-treemacs lsp-ui lsp-mode auto-package-update iedit multiple-cursors expand-region beacon org-bullets undo-tree which-key use-package try ivy-rich helpful counsel ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
